3a639a79e0e393217b0e65fb271e66a42a821bca
siaach <anastia961@gmail.com>
2020-05-07 12:04:29 +0300

    Лабораторная работа 0

---
+++ Lab_0/app/src/main/java/com/example/lab_0/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.lab_0;
+
+import androidx.appcompat.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        //заглушка
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Кукусики)");
+    }
+}
